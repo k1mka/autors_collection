22ca8461109e0823cc4035f79282bcba6a43db4f
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-09-29 11:34:46 +0300

    created the ListTitlesAuthorsScreen, and ListTitlesAuthorsLayout

---
+++ lib/presentation/screens/list_titles_authors_screen/list_titles_authors_screen.dart
@@ -0,0 +1,11 @@
+import 'package:autors_collection/presentation/screens/list_titles_authors_screen/list_titles_authors_layout.dart';
+import 'package:flutter/material.dart';
+
+class ListTitlesAuthorsScreen extends StatelessWidget {
+  const ListTitlesAuthorsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const ListTitlesAuthorsLayout();
+  }
+}
