1af4a38ea8546e3dda5430b85decc1a1871569f6
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-09-29 17:05:06 +0300

    added setupServiceLocator to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:authors_collection/authors_collection_app.dart';
 import 'package:flutter/material.dart';
 
+import 'get_it.dart';
+
 void main() {
+  setupServiceLocator();
   runApp(const AuthorsCollectionsApp());
 }
