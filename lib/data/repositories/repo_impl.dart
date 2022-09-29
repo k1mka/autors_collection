8758f8879d784fb59155669c3e81581f4106b3e4
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-09-29 16:49:00 +0300

    created Repository

---
+++ lib/data/repositories/repo_impl.dart
@@ -0,0 +1,12 @@
+import 'package:authors_collection/data/repositories/repository.dart';
+import 'package:authors_collection/data/services/mocked_data_service.dart';
+import 'package:authors_collection/presentation/models/post_model.dart';
+
+class RepositoryImpl implements Repository {
+  final DataService dataService;
+
+  RepositoryImpl(this.dataService);
+
+  @override
+  List<PostModel> fetchPost() => dataService.fetchPost();
+}
