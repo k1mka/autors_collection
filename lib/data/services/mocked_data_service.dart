84227abe6d40679dbb8008c286bbc42af3939600
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-09-29 15:10:33 +0300

    created data service for mocked data

---
+++ lib/data/services/mocked_data_service.dart
@@ -0,0 +1,25 @@
+import 'package:authors_collection/presentation/models/post_model.dart';
+
+abstract class DataService {
+  List<PostModel> fetchPost();
+}
+
+class MockedDataService implements DataService {
+  final mockedTitle = 'Some Title';
+  final mockedAuthor = 'Some Name';
+  final mockedIcon =
+      'https://images.unsplash.com/photo-1664393633748-19ed89efdb4d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjc4ODZ8MHwxfGFsbHw1fHx8fHx8Mnx8MTY2NDQzNzI4OQ&ixlib=rb-1.2.1&q=80&w=200';
+
+  final mockedFull =
+      'https://images.unsplash.com/photo-1664393633748-19ed89efdb4d?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzNjc4ODZ8MHwxfGFsbHw1fHx8fHx8Mnx8MTY2NDQzNzI4OQ&ixlib=rb-1.2.1&q=80';
+
+  @override
+  List<PostModel> fetchPost() => [
+        for (var i = 0; i < 10; i++)
+          PostModel(
+              title: mockedTitle,
+              author: mockedAuthor,
+              photoIcon: mockedIcon,
+              photoFull: mockedFull)
+      ];
+}
